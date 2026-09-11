3f87477188df89aa19457563bb8dc57c23f9767e
25030252nes <25030252@itesa.edu.mx>
2026-09-10 22:18:21 -0600

    Agregar archivos de proyecto.

---
+++ Stack DTA/Pila.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack_DTA
+{
+    internal class Pila
+    {
+        int[] datos = new int[3];
+        int tope = -1;
+
+        public void Push(int x) => datos[++tope] = x;
+
+        public bool Buscar(int x)
+        {
+            for (int i = 0; i <= tope; i++)
+                if (datos[i] == x) return true;
+            return false;
+        }
+    }
+}
---
+++ Stack DTA/Program.cs
@@ -0,0 +1,11 @@
+﻿using Stack_DTA;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Pila p = new Pila();
+        p.Push(10); p.Push(20); p.Push(30);
+        Console.WriteLine(p.Buscar(20));
+    }
+}
